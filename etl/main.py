cd478e3a9280eaabacfd5eb03dc783a176721cd1
Gwellir <gwellir@gmail.com>
2023-09-09 09:10:20 +0300

    Доработан основной скрипт

--- etl/main.py
+++ etl/main.py
@@ -6,9 +6,13 @@ from loader.elastic_load import ElasticLoader
 from transformer.pg_to_elastic import PostgresElasticTransformer
 
 if __name__ == '__main__':
-    pg_extractor = PostgresExtractor(chunk_size=settings.CHUNK_SIZE)
+    pg_extractor = PostgresExtractor(chunk_size=settings.chunk_size)
     pg_elastic_transformer = PostgresElasticTransformer()
-    elastic_loader = ElasticLoader()
+    elastic_loader = ElasticLoader(
+        settings.elastic_url,
+        settings.elastic_index,
+    )
+    elastic_answers = []
     for pg_data in pg_extractor.extract():
-        elastic_data = pg_elastic_transformer.transform(pg_data)
-        elastic_loader.load(elastic_data)
+        for elastic_data in pg_elastic_transformer.transform(pg_data):
+            elastic_answers.extend(elastic_loader.load(elastic_data))
